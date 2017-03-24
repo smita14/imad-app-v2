705bf878d7e3e6bc9a5178d3b278f61ee858695b
smita14 <smita.chaubey@gmail.com>
2017-03-24 20:59:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send('article-one will be served here');
+  res.send('article-one is requested and will be served here');
 });
 app.get('/article-two', function (req, res) {
-  res.send('article-two will be served here');
+  res.send('article-two is requested and will be served here');
 });
 app.get('/artcle-three', function (req, res) {
-  res.send('article-three will be served here');
+  res.send('article-three is requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
